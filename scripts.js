ad88ae0640f34d8a8ce6f442f21d2663a802347b
mikailaliyev <mikail.aliyev@gmail.com>
2022-07-20 22:22:23 +0400

    event listener added

--- scripts.js
+++ scripts.js
@@ -2,3 +2,18 @@ const selectedOperation = document.getElementById("operations")
 const inputText = document.getElementById("input-text")
 const resultText = document.getElementById("result-text")
 
+//Listening to operation selection
+selectedOperation.addEventListener("change", () => {
+    let operationText = selectedOperation.options[selectedOperation.selectedIndex].text
+    switch(operationText) {
+        case "All letters UPPERCASE":
+            allUpperCase();
+            break;
+        case "All letters lowercase":
+            allLowerCase();
+            break;
+        case "Capitalize":
+            capitalize();
+            break;
+    }
+})
