0d8f81cc7df97205b38dbeb07b05ffea38080691
mikailaliyev <mikail.aliyev@gmail.com>
2022-07-20 22:36:29 +0400

    operation functions added

--- scripts.js
+++ scripts.js
@@ -2,6 +2,25 @@ const selectedOperation = document.getElementById("operations")
 const inputText = document.getElementById("input-text")
 const resultText = document.getElementById("result-text")
 
+//Operation functions
+const allUpperCase = () => {
+    resultText.textContent = inputText.value.toUpperCase()
+    inputText.value = ''
+    selectedOperation.text = ''
+}
+
+const allLowerCase = () => {
+    resultText.textContent = inputText.value.toLowerCase()
+    inputText.value = ''
+    selectedOperation.text = ''
+}
+
+const capitalize = () => {
+    resultText.textContent = inputText.value.charAt(0).toUpperCase() + inputText.value.slice(1)
+    inputText.value = ''
+    selectedOperation.text = ''
+}
+
 //Listening to operation selection
 selectedOperation.addEventListener("change", () => {
     let operationText = selectedOperation.options[selectedOperation.selectedIndex].text
