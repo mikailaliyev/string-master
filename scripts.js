d982a0b0958096e48396b97e2e347dc0867491bf
mikailaliyev <mikail.aliyev@gmail.com>
2022-07-21 22:59:57 +0400

    new operations&template functions added

--- scripts.js
+++ scripts.js
@@ -1,38 +1,130 @@
-const selectedOperation = document.getElementById("operations")
-const inputText = document.getElementById("input-text")
-const resultText = document.getElementById("result-text")
+const selectedOperation = document.getElementById("operations");
+const inputText = document.getElementById("input-text");
+const resultText = document.getElementById("result-text");
+const fromInputText = document.getElementById("from-input-text");
+
+//Template for creating <br> with appending elements to DOM
+const appendElements = (target, text, operation) => {
+  target.append(text);
+  target.append(document.createElement("br"));
+  if (operation) {
+    target.append(operation);
+  } else {
+    target.append(inputText.value);
+  }
+};
+
+//Reseting input and clearing last operation chosed
+const resetInputOperations = () => {
+  inputText.value = "";
+  document.getElementById("operations").value = "0";
+};
 
 //Operation functions
 const allUpperCase = () => {
-    resultText.textContent = inputText.value.toUpperCase()
-    inputText.value = ''
-    selectedOperation.text = ''
-}
+  if (inputText.value) {
+    appendElements(fromInputText, "Input:");
+    appendElements(resultText, "Result:", inputText.value.toUpperCase());
+  }
+  resetInputOperations();
+};
 
 const allLowerCase = () => {
-    resultText.textContent = inputText.value.toLowerCase()
-    inputText.value = ''
-    selectedOperation.text = ''
-}
+  if (inputText.value) {
+    appendElements(fromInputText, "Input:");
+    appendElements(resultText, "Result:", inputText.value.toLowerCase());
+  }
+  resetInputOperations();
+};
 
 const capitalize = () => {
-    resultText.textContent = inputText.value.charAt(0).toUpperCase() + inputText.value.slice(1)
-    inputText.value = ''
-    selectedOperation.text = ''
-}
+  if (inputText.value) {
+    appendElements(fromInputText, "Input:");
+    appendElements(
+      resultText,
+      "Result:",
+      inputText.value.charAt(0).toUpperCase() + inputText.value.slice(1)
+    );
+  }
+  resetInputOperations();
+};
+
+const vowelConsonantCount = () => {
+  if (inputText.value) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    const consonants = [
+      "b",
+      "c",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    let countVowels = 0;
+    let countConsonants = 0;
+    for (input in inputText.value) {
+      if (vowels.includes(inputText.value[input].toLowerCase())) {
+        countVowels++;
+      } else if (consonants.includes(inputText.value[input].toLowerCase())) {
+        countConsonants++;
+      }
+    }
+
+    appendElements(fromInputText, "Input:");
+    appendElements(
+      resultText,
+      "Result:",
+      `Vowels count: ${countVowels}   Consonants count: ${countConsonants}`
+    );
+  }
+  resetInputOperations();
+};
+
+const charactersCount = () => {
+  if (inputText.value) {
+    appendElements(fromInputText, "Input:");
+    appendElements(resultText, "Result:", inputText.value.length);
+  }
+  resetInputOperations();
+};
 
 //Listening to operation selection
 selectedOperation.addEventListener("change", () => {
-    let operationText = selectedOperation.options[selectedOperation.selectedIndex].text
-    switch(operationText) {
-        case "All letters UPPERCASE":
-            allUpperCase();
-            break;
-        case "All letters lowercase":
-            allLowerCase();
-            break;
-        case "Capitalize":
-            capitalize();
-            break;
-    }
-})
+  let operationText =
+    selectedOperation.options[selectedOperation.selectedIndex].text;
+  switch (operationText) {
+    case "All letters UPPERCASE":
+      allUpperCase();
+      break;
+    case "All letters lowercase":
+      allLowerCase();
+      break;
+    case "Capitalize":
+      capitalize();
+      break;
+    case "Vowels&Consonants count":
+      vowelConsonantCount();
+      break;
+    case "Characters count":
+      charactersCount();
+      break;
+    case "Replace Characters":
+      replaceCharacters();
+      break;
+  }
+});
